553a0321854d7cd369f473be886fea110fc7aea0
Manoj Priyanjana <manojpriyanjana@gmail.com>
2023-08-05 02:48:52 +0530

    40. Intents - Moving to a new Activity

--- TheHungryDeveloper/app/src/main/java/com/example/thehungrydeveloper/MainActivity.java
+++ TheHungryDeveloper/app/src/main/java/com/example/thehungrydeveloper/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.thehungrydeveloper;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     CardView startersCard;
@@ -15,5 +17,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         startersCard = findViewById(R.id.card_view_starters);
         mainCards = findViewById(R.id.card_view_mains);
+
+
+        startersCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startersActivityIntent = new Intent(MainActivity.this,StarterActivity.class);
+                startActivity(startersActivityIntent);
+            }
+        });
     }
 }
