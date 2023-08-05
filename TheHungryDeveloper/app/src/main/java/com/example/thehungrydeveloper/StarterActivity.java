1ca333ba2388c205ce9682b3934598c347b9385e
Manoj Priyanjana <manojpriyanjana@gmail.com>
2023-08-05 11:41:34 +0530

    44.creating the array for the starter

--- TheHungryDeveloper/app/src/main/java/com/example/thehungrydeveloper/StarterActivity.java
+++ TheHungryDeveloper/app/src/main/java/com/example/thehungrydeveloper/StarterActivity.java
@@ -14,5 +14,28 @@ public class StarterActivity extends AppCompatActivity {
 
         ListView startersList = findViewById(R.id.list_view_starters);
 
+        String[] dishes = {
+                "Mushroom and tofu maki",
+                "Egg and avocado uramaki",
+                "Melon and lemon soup",
+                "Coconut and chocolate mousse",
+                "Spinach and cabbage wontons",
+                "Broccoli and cucumber soup",
+                "Chilli and aubergine dip",
+                "Chickpea and chilli gyoza",
+                "Sprout and pineapple soup",
+                "Egusi and borscht soup",
+                "Aubergine and egg sushi",
+                "Artichoke and mustard soup",
+                "Peppercorn and tamarind soup",
+                "Parsley and celeriac parcels",
+                "Pasta and broccoli soup",
+                "Potato and courgette soup",
+                "Chickpea and cabbage parcels",
+                "Coriander and peppercorn gyoza",
+                "Pear and chestnut soup",
+                "Pesto and garam masala parcels"
+        };
+
     }
 }
